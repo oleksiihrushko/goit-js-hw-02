e1157803fa65119bc92c541ec19cea936d5e21e5
hrushkooleksii <hrushkooleksii@gmail.com>
2020-04-16 10:04:05 +0300

    fix task-06

--- js/task-06.js
+++ js/task-06.js
@@ -3,15 +3,18 @@ const numbers = [];
 let total = 0;
 do {
   input = prompt('Введите число');
-  Number.isNaN(Number(input))
-    ? alert('Было введено не число, попробуйте еще раз')
-    : numbers.push(Number(input));
+  if (input === null && numbers.length === 0) {
+    alert('Массив пустой');
+  } else {
+    Number.isNaN(Number(input))
+      ? alert('Было введено не число, попробуйте еще раз')
+      : numbers.push(Number(input));
+  }
 } while (input !== null);
 
 if (numbers.length > 0) {
   for (let number of numbers) {
     total += number;
   }
+  alert(`Общая сумма чисел равна ${total}`);
 }
-
-alert(`Общая сумма чисел равна ${total}`);
